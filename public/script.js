44604196c4ebcb0652a4d0aa58ab96394ce3b786
tanthanh-cityboi <tanthanhe@gmail.com>
2022-12-31 16:18:00 +0700

    fix bug

--- public/script.js
+++ public/script.js
@@ -5,14 +5,14 @@ const videoGrid = document.getElementById('video-grid')
 //     host: '/',
 //     port: '3001'
 // })
-const myPeer = new Peer()    
+const myPeer = new Peer()
 
 const peers = {}
 const myVideo = document.createElement('video')
 myVideo.muted = true
 
 navigator.mediaDevices.getUserMedia({
-    // video: true,
+    video: true,
     audio: true
 }).then(stream => {
     addVideoStream(myVideo, stream)
@@ -32,7 +32,7 @@ navigator.mediaDevices.getUserMedia({
 
 socket.on('user-disconnected', (userId) => {
     console.log('User disconnected: ', userId)
-    if(peers[userId]) peers[userId].close()
+    if (peers[userId]) peers[userId].close()
 })
 
 myPeer.on('open', id => {
@@ -48,12 +48,13 @@ function connectToNewUser(userId, stream) {
     call.on('close', () => {
         video.remove()
     })
-    peers[userId] = call;
+
+    peers[userId] = call
 }
 
 function addVideoStream(video, stream) {
-    video.srcObject = stream;
-    video.addEventListener('loadmetadata', () => {
+    video.srcObject = stream
+    video.addEventListener('loadedmetadata', () => {
         video.play()
     })
     videoGrid.append(video)
